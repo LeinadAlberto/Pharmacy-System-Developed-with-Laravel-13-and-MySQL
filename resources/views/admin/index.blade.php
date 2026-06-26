f6c36a242f86bc6616026fa33a1e19543e7233bc
Alberto <dany.canaviri2022@gmail.com>
2026-06-25 23:43:33 -0400

    Admin Controller, Main Layout, and Global Settings

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="page-heading">
+        <h3>Panel de Control</h3>
+    </div>
+
+    <div class="page-content">
+        <p>Bienvenido al Panel de Control</p>
+    </div>
+@endsection
